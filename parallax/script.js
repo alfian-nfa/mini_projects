34d061994628dbceb7cf400f5f34e9632bf94219
Alfian Azis <alfian.azis95@gmail.com>
2024-01-08 16:36:02 +0700

    update content parallax update

--- parallax/script.js
+++ parallax/script.js
@@ -1,10 +1,10 @@
-window.addEventListener("scroll", function () {
-  let offset = window.scrollY;
-  const layers = document.querySelectorAll(".layer");
+// window.addEventListener("scroll", function () {
+//   let offset = window.scrollY;
+//   const layers = document.querySelectorAll(".layer");
 
-  layers.forEach(function (layer, index) {
-    const speed = 0.2 * (index + 1); // Menyesuaikan kecepatan lapisan
-    const yPos = -(offset * speed);
-    layer.style.transform = `translateY(${yPos}px)`;
-  });
-});
+//   layers.forEach(function (layer, index) {
+//     const speed = 0.2 * (index + 1); // Menyesuaikan kecepatan lapisan
+//     const yPos = -(offset * speed);
+//     layer.style.transform = `translateY(${yPos}px)`;
+//   });
+// });
