03c13b9e2b0ac12c85aa38e2cf1ce9fd05e87213
Alfian Azis <alfian.azis95@gmail.com>
2024-01-16 13:52:52 +0700

    update content add parallax

--- parallax/script.js
+++ parallax/script.js
@@ -1,10 +1,45 @@
-// window.addEventListener("scroll", function () {
-//   let offset = window.scrollY;
-//   const layers = document.querySelectorAll(".layer");
+// window.addEventListener("scroll", function (event) {
+//   var top = window.scrollY;
 
-//   layers.forEach(function (layer, index) {
-//     const speed = 0.2 * (index + 1); // Menyesuaikan kecepatan lapisan
-//     const yPos = -(offset * speed);
-//     layer.style.transform = `translateY(${yPos}px)`;
-//   });
+//   var layers = document.getElementsByClassName("parallax");
+//   var layer, speed, yPos;
+//   for (var i = 0; i < layers.length; i++) {
+//     layer = layers[i];
+//     speed = layer.getAttribute("data-speed");
+//     var yPos = -((top * speed) / 100);
+//     layer.setAttribute("style", "transform: translate3d(0px, " + yPos + "px, 0px)");
+//   }
 // });
+
+window.addEventListener("scroll", function () {
+  let offset = window.scrollY;
+  const layers = document.querySelectorAll(".parallax");
+
+  layers.forEach(function (layer, index) {
+    const speed = 0.175 * (index + 1); // Menyesuaikan kecepatan lapisan
+    const yPos = -(offset * speed);
+    layer.style.transform = `translateY(${yPos}px)`;
+  });
+});
+
+window.addEventListener("scroll", function () {
+  let offset = window.scrollY;
+  const clouds = document.querySelectorAll(".cloudRight");
+
+  clouds.forEach(function (layer, index) {
+    const speedX = 0.1 * (index + 1); // Menyesuaikan kecepatan lapisan
+    const xPos = offset * speedX;
+    layer.style.transform = `translateX(${xPos}px)`;
+  });
+});
+
+window.addEventListener("scroll", function () {
+  let offset = window.scrollY;
+  const clouds = document.querySelectorAll(".cloudLeft");
+
+  clouds.forEach(function (layer, index) {
+    const speedX = 0.1 * (index + 1); // Menyesuaikan kecepatan lapisan
+    const xPos = -(offset * speedX);
+    layer.style.transform = `translateX(${xPos}px)`;
+  });
+});
